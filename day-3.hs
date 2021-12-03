470b96a1e3d05e9edc4367fea4c5afd5435e8332
Sean Nyman <snyman.git@gmail.com>
2021-12-03 02:50:38 -0500

    2021-D3B Use contextual filtering to find the right bits
    Basically using a tree to count prefixes - i.e. the count of a tree's
    node is the number of entries that have the same prefix as you would use
    to navigate to that node from the root. So the root node is the total
    number (since all entries have the empty prefix). The root's first and
    second children has the number of entries that start with 1 and 0,
    respectively, and so on.
    
    This works, but turns out to be a bit awkward to then traverse, since
    you're effectively rebuilding the prefix, which requires knowledge of
    the children at that level (e.g. at the root node, to know if the first
    element in the prefix is a 1 or a 0 you need to check how big the
    children are, and possibly if they even exist or are just leaves).

--- day-3.hs
+++ day-3.hs
@@ -1,6 +1,8 @@
 #!/usr/bin/env stack
 -- stack --resolver=lts-18.18 script
 
+data BinTree = Leaf | Node Int BinTree BinTree deriving Show
+
 main :: IO ()
 main = do
   diagnostics <- getContents >>= return . lines
@@ -9,6 +11,12 @@ main = do
   putStrLn $ show gamma
   putStrLn $ show epsilon
   putStrLn $ show ((toDecimal gamma) * (toDecimal epsilon))
+  let popTree = buildTree $ map bits diagnostics
+  let oxy = oxygenRate popTree
+  let co2 = co2Rate popTree
+  putStrLn $ show $ oxy
+  putStrLn $ show $ co2
+  putStrLn $ show ((toDecimal oxy) * (toDecimal co2))
 
 bits :: String -> [Bool]
 bits ('1':bs) = True:bits bs
@@ -29,3 +37,32 @@ combine ns bs = map (\(n, b) -> if b then n + 1 else n - 1) $ zip ns bs
 
 commonBits :: [Int] -> [Bool]
 commonBits = map (>0)
+
+buildTree :: [[Bool]] -> BinTree
+buildTree = foldl add Leaf
+
+add :: BinTree -> [Bool] -> BinTree
+add (Node n ts fs) (True:bs) = Node (n + 1) (ts `add` bs) fs
+add (Node n ts fs) (False:bs) = Node (n + 1) ts (fs `add` bs)
+add (Node n ts fs) [] = Node (n + 1) ts fs
+add Leaf bs = (Node 0 Leaf Leaf) `add` bs
+
+size :: BinTree -> Int
+size (Node n _ _) = n
+size Leaf = 0
+
+oxygenRate :: BinTree -> [Bool]
+oxygenRate (Node _ Leaf Leaf) = []
+oxygenRate (Node _ t f)
+  | size t >= size f = True:oxygenRate t
+  | otherwise       = False:oxygenRate f
+oxygenRate Leaf = []
+
+co2Rate :: BinTree -> [Bool]
+co2Rate (Node _ Leaf Leaf) = []
+co2Rate (Node _ t Leaf) = True:co2Rate t
+co2Rate (Node _ Leaf f) = False:co2Rate f
+co2Rate (Node _ t f)
+  | size t < size f = True:co2Rate t
+  | otherwise       = False:co2Rate f
+co2Rate Leaf = []
