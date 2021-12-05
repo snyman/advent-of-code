1ba2916f831967a6bfc737dc174808d46443826b
Sean Nyman <snyman.git@gmail.com>
2021-12-05 01:25:54 -0500

    2021-D5B Handle diagonal vents as well
    Also realized I should have calculated the points covered by a vent more
    elegantly in the first place. Having to use a combinatorial number of
    guard expressions probably should have been a red flag.

--- day-5.hs
+++ day-5.hs
@@ -11,8 +11,9 @@ main = do
   let vents = map parseVent ventLines
   let orthogonalVents = filter isOrthogonal vents
   mapM_ putStrLn $ map show orthogonalVents
-  let insPts = intersections $ allPoints orthogonalVents
-  putStrLn $ show insPts
+  let insOrthoPts = intersections $ allPoints orthogonalVents
+  putStrLn $ show $ length insOrthoPts
+  let insPts = intersections $ allPoints vents
   putStrLn $ show $ length insPts
 
 data Vent = Vent {x1 :: Int, y1 :: Int, x2 :: Int, y2 :: Int} deriving (Show, Eq)
@@ -25,15 +26,13 @@ isOrthogonal :: Vent -> Bool
 isOrthogonal v = (x1 v == x2 v) || (y1 v == y2 v)
 
 points :: Vent -> [(Int, Int)]
-points v
-  | xa == xb && ya <= yb = zip [xa,xb..] [ya..yb]
-  | xa == xb && ya > yb = zip [xa,xb..] [yb..ya]
-  | xa <= xb && ya == yb = zip [xa..xb] [ya,yb..]
-  | xa > xb && ya == yb = zip [xb..xa] [ya,yb..]
-  where xa = x1 v
-        xb = x2 v
-        ya = y1 v
-        yb = y2 v
+points (Vent x1 y1 x2 y2) = zip (range x1 x2) (range y1 y2)
+
+range :: Int -> Int -> [Int]
+range a b
+  | a == b = [a,a..]
+  | a < b = [a..b]
+  | a > b = [a,a-1..b]
 
 intersections :: M.Map (Int, Int) Int -> [(Int, Int)]
 intersections m = M.foldlWithKey addIntersection [] m
