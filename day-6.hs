9c885e4a22cbf91601ddf31bff789f0f9e679f42
Sean Nyman <snyman.git@gmail.com>
2021-12-06 00:52:14 -0500

    2021-D6B Counting a lot more laternfish
    So as alluded to in the previous commit, the new goal of calculating the
    fish population 256 days in the future is too much for the naive
    approach. Something a bit more efficient is needed.
    
    Taking a look at a single fish, the number of fish it will be an
    ancestor to after a given number of days can be expressed as a
    recurrence formula. And what's a great way to efficiently compute
    recurrence formulae? Dynamic programming! And as it turns out, Haskell's
    laziness makes this rather easy, assuming you know the trick to it. You
    simply define a list or array or whatever recursively, following the
    recurrence relation. The list is lazy, so any particular value won't be
    computed until called for, but it also won't be computed more than once.
    And so you get an efficient solution that doesn't look much different
    than the naive implementation. Yay!

--- day-6.hs
+++ day-6.hs
@@ -1,15 +1,18 @@
 #!/usr/bin/env stack
--- stack --resolver=lts-18.18 script --package split
+-- stack --resolver=lts-18.18 script --package split --package array
 
 import Data.List.Split
+import Data.Array((!), listArray)
 
 main :: IO ()
 main = do
   initialFish <- fmap parseFish $ getContents
   putStrLn $ show $ initialFish
   putStrLn $ show $ simulateDay initialFish
+  putStrLn $ show $ simulate 1 initialFish
   putStrLn $ show $ simulate 18 initialFish
-  putStrLn $ show $ length $ simulate 80 initialFish
+  putStrLn $ show $ simulate 80 initialFish
+  putStrLn $ show $ simulate 256 initialFish
 
 parseFish :: String -> [Int]
 parseFish = map read . splitOn ","
@@ -19,6 +22,18 @@ simulateDay (0:fs) = 6:8:simulateDay fs
 simulateDay (x:fs) = (x-1):simulateDay fs
 simulateDay [] = []
 
-simulate :: Int -> [Int] -> [Int]
-simulate 0 fs = fs
-simulate x fs = simulate (x-1) $ simulateDay fs
+simulate :: Int -> [Int] -> Int
+simulate d fs = sum $ map (simulateSingle d) fs
+
+simulateSingle :: Int -> Int -> Int -- days -> fish -> fishCount
+simulateSingle d f = 1 + spawnCount (d - f)
+
+spawnCount :: Int -> Int -- days -> fishCount (for single fish at 0)
+spawnCount n
+  | n >= 0 = r!n
+  | otherwise = 0
+  where
+    r = listArray (0, n) (map f [0..n])
+    f 0 = 0
+    f i = 1 + lookup (i - 7) + lookup (i - 9)
+    lookup i = if i < 0 then 0 else r!i
