ac8d0f01499ecc04ab6665feaf90c19ede9f7ee4
Sean Nyman <snyman.git@gmail.com>
2021-12-08 01:22:44 -0500

    2021-D8B Fully decoding the mixed wires
    There may be a more elegant way to solve this, but the first thing that
    sprung to my mind was to see if the number of digits a given wire
    (letter) appeared in would be helpful, and it was. Of the 7 wires, 3 can
    be identified by the number of digits they appear in, and the other 4
    are paired up in a way that lets me identify them using the method from
    part one. e.g., both a and c appear in 8 digits, but only c appears in
    the combination for "1" (which uniquely uses two wires). Figuring out
    how to code this was a bit difficult, and I don't think it turned out
    especially elegant, but it was still fairly easy to do.

--- day-8.hs
+++ day-8.hs
@@ -1,25 +1,84 @@
 #!/usr/bin/env stack
--- stack --resolver=lts-18.18 script --package split
+-- stack --resolver=lts-18.18 script --package split --package containers
 
 import Data.List.Split
 import Data.Maybe
+import qualified Data.Map as M
+import Data.List
 
 main :: IO ()
 main = do
   lines <- fmap lines getContents
-  let outputs = map parseSignals lines
-  putStrLn $ show $ outputs
-  putStrLn $ show $ countDigits outputs
+  let signals = map parseSignals lines
+  let outputs = map computeOutput signals
+  putStrLn $ show $ sum outputs
 
-parseSignals :: String -> [String]
-parseSignals = words . (!!1) . splitOn "|"
+parseSignals :: String -> ([String], [String])
+parseSignals s = (patterns s, outputs s)
+    where patterns = words . (!!0) . splitOn "|"
+          outputs = words . (!!1) . splitOn "|"
 
 countDigits :: [[String]] -> Int
-countDigits signals = length $ catMaybes $ concatMap (\ss -> map parseDigit ss) signals
+countDigits signals = length $ catMaybes $ concatMap (\ss -> map parseScrambledDigit ss) signals
 
-parseDigit :: String -> Maybe Int
-parseDigit [_,_] = Just 1
-parseDigit [_,_,_,_] = Just 4
-parseDigit [_,_,_] = Just 7
-parseDigit [_,_,_,_,_,_,_] = Just 8
-parseDigit _ = Nothing
+parseScrambledDigit :: String -> Maybe Int
+parseScrambledDigit [_,_] = Just 1
+parseScrambledDigit [_,_,_,_] = Just 4
+parseScrambledDigit [_,_,_] = Just 7
+parseScrambledDigit [_,_,_,_,_,_,_] = Just 8
+parseScrambledDigit _ = Nothing
+
+computeOutput :: ([String], [String]) -> Int
+computeOutput (patterns, outputs) = read $ concatMap (show . parseDigit) descrambledOutputs
+    where descrambledOutputs = unscrambleSignals wireMapping outputs
+          wireMapping = decodeWires patterns
+
+parseDigit :: String -> Int
+parseDigit "abcefg" = 0
+parseDigit "cf" = 1
+parseDigit "acdeg" = 2
+parseDigit "acdfg" = 3
+parseDigit "bcdf" = 4
+parseDigit "abdfg" = 5
+parseDigit "abdefg" = 6
+parseDigit "acf" = 7
+parseDigit "abcdefg" = 8
+parseDigit "abcdfg" = 9
+
+unscrambleSignals :: M.Map Char Char -> [String] -> [String]
+unscrambleSignals wireMapping words = map (sort . descramble) words
+    where descramble = map (wireMapping M.!)
+
+decodeWires :: [String] -> M.Map Char Char
+decodeWires words = M.fromList $ map (decodeWire words) ['a'..'g']
+
+decodeWire :: [String] -> Char -> (Char, Char)
+decodeWire words letter =
+    case wordsWithLetter letter words of
+        8 -> decideAOrC letter words
+        6 -> (letter, 'b')
+        7 -> decideDOrG letter words
+        4 -> (letter, 'e')
+        9 -> (letter, 'f')
+
+decideAOrC :: Char -> [String] -> (Char, Char)
+decideAOrC letter words = if letter `elem` one then (letter, 'c') else (letter, 'a')
+    where one = fromJust $ find ((==2) . length) words
+
+decideDOrG :: Char -> [String] -> (Char, Char)
+decideDOrG letter words = if letter `elem` four then (letter, 'd') else (letter, 'g')
+    where four = fromJust $ find ((==4) . length) words
+
+wordsWithLetter :: Char -> [String] -> Int
+wordsWithLetter l s = length $ filter (elem l) s
+
+-- a - 8
+-- b - 6
+-- c - 8
+-- d - 7
+-- e - 4
+-- f - 9
+-- g - 7
+
+-- a is not in 1 but c is
+-- d is in 4 and g isn't
