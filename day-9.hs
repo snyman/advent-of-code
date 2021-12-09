2a51320f98e12df7a86aacadba43b80724fe3d51
Sean Nyman <snyman.git@gmail.com>
2021-12-09 01:27:02 -0500

    2021-D9B Flood-fill to find basins

--- day-9.hs
+++ day-9.hs
@@ -2,13 +2,16 @@
 -- stack --resolver=lts-18.18 script --package split --package containers --package array
 
 import Data.List
+import Data.Ord
 import Data.Array.IArray
+import qualified Data.Set as S
 
 main :: IO ()
 main = do
   floorDepths <- fmap parseInput getContents
   putStrLn $ show floorDepths
   putStrLn $ show $ calculateRisk floorDepths
+  putStrLn $ show $ largestBasins floorDepths 3
 
 type DepthMap = Array (Int, Int) Int
 
@@ -20,7 +23,7 @@ parseInput s = array ((1,1), (colCount, rowCount)) $ concat [[ ((x, y), read $ i
     colCount = length first
 
 isLowPoint :: DepthMap -> (Int, Int) -> Bool
-isLowPoint m (x, y) = all id $ map (isLower m (x, y)) [(x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)]
+isLowPoint m p = all id $ map (isLower m p) [north p, east p, south p, west p]
 
 riskLevel :: Int -> Int
 riskLevel = (+1)
@@ -32,3 +35,32 @@ isLower m p p'
 
 calculateRisk :: DepthMap -> Int
 calculateRisk m = sum $ map (riskLevel . (m!)) $ filter (isLowPoint m) $ indices m
+
+basin :: DepthMap -> (Int, Int) -> S.Set (Int, Int)
+basin m p
+  | inRange (bounds m) p && m ! p < 9 = S.unions [ S.singleton p, northB, eastB, southB, westB ]
+  | otherwise = S.empty
+  where
+    subBasin dir = let p' = dir p in if isLower m p p' then basin m p' else S.empty
+    northB = subBasin north
+    eastB = subBasin east
+    southB = subBasin south
+    westB = subBasin west
+
+north :: (Int, Int) -> (Int, Int)
+north (x, y) = (x, y-1)
+
+south :: (Int, Int) -> (Int, Int)
+south (x, y) = (x, y+1)
+
+east :: (Int, Int) -> (Int, Int)
+east (x, y) = (x+1, y)
+
+west :: (Int, Int) -> (Int, Int)
+west (x, y) = (x-1, y)
+
+allBasins :: DepthMap -> [S.Set (Int, Int)]
+allBasins m = map (basin m) $ filter (isLowPoint m) $ indices m
+
+largestBasins :: DepthMap -> Int -> Int
+largestBasins m n = product $ take n $ sortOn Down $ map S.size $ allBasins m
