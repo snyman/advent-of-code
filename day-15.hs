521de4f601e5981438f68ce1eddfdaa0eb426fca
Sean Nyman <snyman.git@gmail.com>
2021-12-15 01:53:52 -0500

    2021-D15A Finding the cheapest path
    So I'm not sure if there's an even better way to do this one, there
    probably is, but I remembered from some algorithms class I once took
    that the A* algorithm is both better time-wise than a brute-force
    approach, and guaranteed to get the least-cost path as long as the
    heuristic (the h(n) function) follows the rule that h(n) is admissible
    (i.e. it will never overestimate the actual least-cost from n to the
    goal). In this case, a fairly trivial (if perhaps non-optimal) h(n) is
    simply the orthogonal distance from n to the goal, as that is the
    minimum number of positions the path must pass through, and each
    position is guaranteed to have a cost >= 1.
    
    There is probably some implementation of the A* algorithm for Haskell,
    but I figured I should probably implement it myself, though it mainly
    consisted of translating the pseudocode from Wikipedia as directly as
    possible.

---
+++ day-15.hs
@@ -0,0 +1,90 @@
+#!/usr/bin/env stack
+-- stack --resolver=lts-18.18 script --package split --package containers --package array --package PSQueue
+
+import Data.List
+import Data.List.Split
+import Data.Array.IArray
+import qualified Data.Map as M
+import qualified Data.PSQueue as PSQ
+
+main :: IO ()
+main = do
+  risk <- fmap parseInput getContents
+  putStrLn $ show risk
+  let path = aStar risk
+  putStrLn $ show $ path
+  putStrLn $ show $ (totalRisk <$> (Just risk) <*> path)
+
+biSplit :: Eq a => [a] -> [a] -> ([a], [a])
+biSplit delim s = (a, concat (b:c))
+  where (a:b:c) = splitOn delim s
+
+type Coord = (Int, Int)
+type RiskMap = Array Coord Int
+
+parseInput :: String -> RiskMap
+parseInput s = array ((1,1), (colCount, rowCount)) $ concat [[ ((x, y), read $ i:[]) | (i, x) <- zip row [1..] ] | (row, y) <- zip rows [1..]]
+  where
+    rows@(first:_) = lines s
+    rowCount = length rows
+    colCount = length first
+
+type Score = M.Map Coord Int
+
+gScore :: Score -> Coord -> Int
+gScore s c = M.findWithDefault maxBound c s
+
+hScore :: RiskMap -> Coord -> Int
+hScore r (x, y) = (maxX - x) + (maxY - y)
+  where (maxX, maxY) = snd $ bounds r
+
+fScore :: Score -> RiskMap -> Coord -> Int
+fScore s r c =
+  case M.lookup c s of
+   (Just gScore) -> gScore + hScore r c
+   Nothing -> maxBound
+
+data AStar = AStar { risk :: RiskMap, score :: Score, from :: M.Map Coord Coord, open :: PSQ.PSQ Coord Int }
+
+aStar :: RiskMap -> Maybe [Coord]
+aStar r = aStar' $ AStar r gScoreMap M.empty openSet
+  where
+    (start, end) = bounds r
+    gScoreMap = M.singleton start 0
+    openSet = PSQ.singleton start $ fScore gScoreMap r start
+
+aStar' :: AStar -> Maybe [Coord]
+aStar' (AStar risk score from open) = do
+  (current, open') <- PSQ.minView open
+  let currentCoord = PSQ.key current
+  let goal = snd $ bounds risk
+  if currentCoord == goal
+    then return $ reconstructPath from currentCoord
+    else do
+      let new = foldl (processNeighbour currentCoord) (AStar risk score from open') $ adjacencies risk currentCoord
+      aStar' new
+
+reconstructPath :: M.Map Coord Coord -> Coord -> [Coord]
+reconstructPath from coord = case (M.lookup coord from) of
+  (Just prev) -> coord:(reconstructPath from prev)
+  Nothing -> [coord]
+
+processNeighbour :: Coord -> AStar -> Coord -> AStar
+processNeighbour current orig@(AStar risk score from open) neighbour =
+  if tentativeScore < (gScore score neighbour) then
+    let from' = M.insert neighbour current from
+        score' = M.insert neighbour tentativeScore score
+        open' = PSQ.insert neighbour (fScore score' risk neighbour) open
+    in AStar risk score' from' open'
+  else orig
+  where
+    tentativeScore = (gScore score current) + (risk ! neighbour)
+
+validPoint :: RiskMap -> Coord -> Bool
+validPoint m = inRange $ bounds m
+
+adjacencies :: RiskMap -> Coord -> [Coord]
+adjacencies m (x, y) = filter (validPoint m) $ map (\(x', y') -> (x + x', y + y')) [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+totalRisk :: RiskMap -> [Coord] -> Int
+totalRisk risk path = foldl (\score coord -> score + (risk ! coord)) (-(risk ! (1, 1)))  path
